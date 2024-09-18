e19e7f28b93c485b2d0ec7c8551d48df7394a45d
Meri Kasprak <mkasprak@mchenry.edu>
2024-09-18 14:02:53 -0500

    loops and bit operators

---
+++ looping.py
@@ -0,0 +1,36 @@
+# please don't do this
+
+# count = 10
+# while count > 0:
+#     print(count)
+#     # count--
+#     # https://chatgpt.com/share/e/66eb1c17-ccf0-800e-86d8-7fd4c28edb40
+#     count = count - 1
+
+
+# in c++
+# for (int i = 0; i >= 0; i++):
+
+# for i in range(1, 13, 2):
+#     print(i)
+
+# for i in range(1, 14):
+#     print(i)
+# weekdays = ("Sunday", "Monday", "Tuesday")
+
+# for day in (weekdays):
+#     print(day)
+
+# for number in [1, 2, 3, 4, 5]:
+#     if number == 3:
+#         continue
+#     print(number)
+
+for i in range(5):
+    if i == 83:
+        break
+    print(i)
+print(i)
+
+else:
+    print("Loop completed without break")
