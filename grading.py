1e65bbb0a41566dc7ccd4431bcccf695201697fa
Meri Kasprak <mkasprak@mchenry.edu>
2024-09-23 14:31:59 -0500

    lists

---
+++ grading.py
@@ -0,0 +1,6 @@
+# Iterate through numbers from 1 to 600
+for number in range(1, 601):
+    # Check if the number is divisble ny 7
+    if number % 7 == 0:
+        # print the number if it is divisble by 7
+        print(number)
