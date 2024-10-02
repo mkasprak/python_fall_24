4d50a3f6938008c6746d764bcda8ce7d6c64130d
Meri Kasprak <mkasprak@mchenry.edu>
2024-10-02 12:52:39 -0500

    I don't know

--- grading.py
+++ grading.py
@@ -1,34 +1,16 @@
-# Wednesday 09/18/2024 3.3A Logical Operations
+# Ticket sales assignment
 
-int1 = int(input("Please enter an integer: "))
-int2 = int(input("Please enter another integer: "))
-
-if int1 >= 16 and int2 >= 16:
-    print("Both numbers are greater than or equal 16")
-else:
-    print("Not all numbers are greater than or equal 16")
-
-if int1 <= 56 and int2 <= 56:
-    print("Both numbers are less than or equal 56")
-else:
-    print("Not all numbers are less then or equal 56")
-
-if (int1 % 2) == 0 or (int2 % 2) == 0:
-    print("Either number is even")
-else:
-    print("Neither number is even")
-
-if (int1 % 7 == 0) or (int2 % 7 == 0):
-    print("Either number is divisible by 7")
-else:
-    print("Neither number is divisible by 7")
-
-if not int1 > int2:
-    print("Integer one is not greater than integer 2.")
-else:
-    print("Integer one is greater than integer 2.")
-
-if not int2 >= 250:
-    print("Integer two is not greater or equal to 250")
-else:
-    print("Integer two is greater than or equal to 250.")
+# Create list
+seats = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+         "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+taken = "0"
+# Start selling tickets
+while taken != "sold out":
+    taken = input("Please input the seat you would like: ")
+    if taken in seats:
+        seats.remove(taken)
+    for item in seats:
+        print(item)
+    if len(seats) == 0:
+        print("We are sold out.")
+        taken = "sold out"
