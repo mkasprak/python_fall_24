7e7f1e0c6966602f3add1d0013ba14f0136a6b9b
Meri Kasprak <mkasprak@mchenry.edu>
2024-12-02 14:32:30 -0600

    calendar

---
+++ my_calendar.py
@@ -0,0 +1,10 @@
+import calendar
+
+
+def main():
+    print(calendar.calendar(2025))
+
+    print(calendar.month(2025, 5))
+
+
+main()
---
+++ my_os.py
@@ -0,0 +1,11 @@
+import os
+
+
+def main():
+    # os.mkdir("ducks")
+    os.mkdir("ducks/real")
+    os.mkdir("ducks/cartoon")
+    os.mkdir("ducks/not_ducks")
+
+
+main()
